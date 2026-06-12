a70f28fd2975c37cf746b629fa14338971d1e1b1
seu nome <seu@email.com>
2026-06-12 14:29:10 -0300

    Corrigindo estrutura e renomeando para index.html

---
+++ enviar_para_github.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Preparando para enviar arquivos para o GitHub...
+echo Vai aparecer uma janela pedindo para voce fazer login no GitHub.
+git push -u origin main
+echo.
+echo Processo finalizado. Se deu tudo certo, pode fechar esta janela.
+pause
